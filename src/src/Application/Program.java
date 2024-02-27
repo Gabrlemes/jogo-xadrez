e6a99c2aad94f1084054688ddd12f85a0b34b13b
Gabriel lemes <gabrielsiber816@gmail.com>
2024-02-27 17:31:53 -0300

    beggin

--- src/src/Application/Program.java
+++ src/src/Application/Program.java
@@ -1,9 +1,12 @@
 package Application;
 
+import tabuleiro.Board;
+import tabuleiro.Position;
+
 public class Program {
 
     public static void main(String[] args) {
 
-        System.out.println("hello");
+        Board board = new Board(8, 8);
     }
 }
